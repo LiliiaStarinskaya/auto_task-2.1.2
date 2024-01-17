372c95758abb54f66d4c3b289309d63cc30c5944
Lstarinskaya <starinskayaliliia@gmail.com>
2024-01-17 04:17:47 +0300

    Added task_score.js for 2.1.3 task

---
+++ task_score.js
@@ -0,0 +1,11 @@
+let introduction = 50;
+let git = 50;
+let js = 100;
+let total = introduction + git + js;
+let average = total / 3;
+let studentName = "Luntik";
+let studentSurname = "Dzishkariani";
+
+console.log(
+  `The average score of ${studentName} ${studentSurname} for the modules is ${average}.`
+);
