68b4ecc6f9673928f2ee35915c3857da2a3154b1
Lstarinskaya <starinskayaliliia@gmail.com>
2024-01-29 04:09:41 +0300

    Edited new_testdata.js for 2.3.1.2 task

--- new_testdata.js
+++ new_testdata.js
@@ -1,31 +1,34 @@
 const players = [
-  { id: 1, name: "John Doe", scorePoints: 85 },
+  { id: 1, name: "", scorePoints: 85 },
   { id: 2, name: "Jane Smith", scorePoints: 92 },
-  { id: 3, name: "Michael Johnson", scorePoints: 78 },
-  { id: 4, name: "Emily Williams", scorePoints: 95 },
+  { id: 3, name: "Michael Johnson", scorePoints: null },
+  { id: 4, name: "", scorePoints: 95 },
   { id: 5, name: "Robert Brown", scorePoints: 88 },
-  { id: 6, name: "Sophia Davis", scorePoints: 91 },
+  { id: 6, name: "@$%#@", scorePoints: 91 },
   { id: 7, name: "Daniel Lee", scorePoints: 87 },
-  { id: 8, name: "Olivia Taylor", scorePoints: 94 },
-  { id: 9, name: "Matthew Anderson", scorePoints: 89 },
-  { id: 10, name: "Ava Martinez", scorePoints: 96 },
+  { id: 8, name: null, scorePoints: 99 },
+  { id: 9, name: 55, scorePoints: 89 },
+  { id: 10, name: "Ava Martinez", scorePoints: "111" },
 ];
-let scorePointsArray = [];
+const invalidCharactersRegex = /[@$%#]/;
+const validPlayers = players.filter(
+  (player) =>
+    player.scorePoints !== null &&
+    !isNaN(player.scorePoints) &&
+    typeof player.scorePoints === "number" &&
+    typeof player.name === "string" &&
+    player.name !== null &&
+    player.name !== "" &&
+    !invalidCharactersRegex.test(player.name)
+);
 
-for (let key in players) {
-  if (players[key].hasOwnProperty("scorePoints")) {
-    scorePointsArray.push(players[key].scorePoints);
-  }
-}
+let scorePointsArray = validPlayers.map((player) => player.scorePoints);
 
-function saveScorePoints(array) {
-  array.forEach((userData) => {});
-}
-saveScorePoints(players);
 let maxScore = Math.max(...scorePointsArray);
 console.log("The maximum score for the course is: " + maxScore);
 
-let studentWithMaxScore = players.find(
+let studentWithMaxScore = validPlayers.find(
   (player) => player.scorePoints === maxScore
 );
-console.log("Student with the maximum score: " + studentWithMaxScore.name);
+
+console.log("Student with the maximum score:" + studentWithMaxScore.name);
