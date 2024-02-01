b811cb2552ac27d85b26955484e98cb3a9a323af
Lstarinskaya <starinskayaliliia@gmail.com>
2024-02-01 23:52:13 +0300

    Added debugger.js for 2.4.4 task

---
+++ debugger.js
@@ -0,0 +1,9 @@
+const calculateBonus = (a, b) => {
+  let bonus;
+  const sum = a + b;
+  debugger; // сумма посчитана, bonus неопределен
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  debugger; //определен bonus, но еще не вернулся
+  return bonus;
+};
+calculateBonus(8, 9);
