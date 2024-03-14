502880b824906f64ff0442db712ac43c21778c93
Lstarinskaya <starinskayaliliia@gmail.com>
2024-03-14 04:07:46 +0300

    Edited 3.2.2.js for 3.2.2 task

--- 3.2.2.js
+++ 3.2.2.js
@@ -1,9 +1,14 @@
-module.exports = function findStudentWithMaxScore(students) {
+module.exports = function findStudentsWithMaxScore(students) {
   const flatStudents = students.flat();
 
   const filteredStudents = flatStudents.filter(
-    (student) => student.date && student.date !== ""
+    (student) => student.date && student.date !== "" && student.score !== 0
   );
+
+  if (filteredStudents.length === 0) {
+    console.log("Студентов с валидными параметрами не найдено.");
+    return [];
+  }
 
   const maxScore = Math.max(
     ...filteredStudents.map((student) => student.score)
@@ -13,13 +18,19 @@ module.exports = function findStudentWithMaxScore(students) {
     (student) => student.score === maxScore
   );
 
-  studentsWithMaxScore.sort((a, b) => a.date.localeCompare(b.date));
-
-  const studentWithMaxScoreAndEarliestDate = studentsWithMaxScore[0];
-
-  console.log(
-    `Поздравляем, ${studentWithMaxScoreAndEarliestDate.name}! Вы набрали больше баллов, чем ваши коллеги. Ваш балл: ${maxScore}`
-  );
-
-  return studentWithMaxScoreAndEarliestDate.name;
+  if (studentsWithMaxScore.length === 1) {
+    const student = studentsWithMaxScore[0];
+    console.log(
+      `Поздравляем,  ${student.name}! Вы набрали больше баллов, чем ваши коллеги. Ваш балл: ${maxScore}`
+    );
+    return [student.name];
+  } else {
+    studentsWithMaxScore.sort((a, b) => a.date.localeCompare(b.date));
+    console.log(
+      `Поздравляем следующих студентов с наивысшим баллом (${maxScore}):`
+    );
+    const names = studentsWithMaxScore.map((student) => student.name);
+    console.log(names.join(", "));
+    return names;
+  }
 };
